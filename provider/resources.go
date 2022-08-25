82a92815895f0906f38c7df1a045214f58ea9e38
Thomas Meckel <tmeckel@users.noreply.github.com>
2022-08-25 15:29:57 +0200

    Added clock logo

--- provider/resources.go
+++ provider/resources.go
@@ -64,7 +64,7 @@ func Provider() tfbridge.ProviderInfo {
 		//
 		// You may host a logo on a domain you control or add an SVG logo for your package
 		// in your repository and use the raw content URL for that file as your logo URL.
-		LogoURL: "",
+		LogoURL: "https://www.svgrepo.com/show/77630/clock.svg",
 		// PluginDownloadURL is an optional URL used to download the Provider
 		// for use in Pulumi programs
 		// e.g https://github.com/org/pulumi-provider-name/releases/
